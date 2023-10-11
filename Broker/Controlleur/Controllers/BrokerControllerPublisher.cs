c16c3660cf72b3faa622fd21301482af0546f46b
DavidDaniel18 <david.vermette-nadeau.1@ens.etsmtl.ca>
2023-10-11 01:07:08 -0400

    update

--- Broker/Controlleur/Controllers/BrokerControllerPublisher.cs
+++ Broker/Controlleur/Controllers/BrokerControllerPublisher.cs
@@ -3,20 +3,25 @@
 using Entities;
 using Interfaces.Domain;
 using Interfaces.Handler;
+using Microsoft.Extensions.Logging;
 using SmallTransit.Abstractions.Broker;
 using SmallTransit.Abstractions.Interfaces;
 
 public sealed class BrokerControllerPublisher : IConsumer<BrokerReceiveWrapper>
 {
     private readonly IPublisherHandler _publisherHandler;
+    private readonly ILogger<BrokerControllerPublisher> _logger;
 
-    public BrokerControllerPublisher(IPublisherHandler publisherHandler) 
-    { 
+    public BrokerControllerPublisher(IPublisherHandler publisherHandler, ILogger<BrokerControllerPublisher> logger)
+    {
         _publisherHandler = publisherHandler;
+        _logger = logger;
     }
 
     public Task Consume(BrokerReceiveWrapper contract)
     {
+        _logger.LogInformation($"Publication attempt for routing key {contract.RoutingKey}");
+
         IPublication publication = Publication.From(contract);
         // Add the topic to the router.
         _publisherHandler.Advertise(publication.RoutingKey);
--- Broker/Controlleur/Controllers/BrokerControllerSubscription.cs
+++ Broker/Controlleur/Controllers/BrokerControllerSubscription.cs
@@ -3,20 +3,25 @@ using Application;
 using Entities;
 using Interfaces.Domain;
 using Interfaces.Handler;
+using Microsoft.Extensions.Logging;
 using SmallTransit.Abstractions.Broker;
 using SmallTransit.Abstractions.Interfaces;
 
 public sealed class BrokerControllerSubscription : IConsumer<BrokerSubscriptionDto>
 {
     private readonly ISubscriptionHandler _subscriptionHandler;
+    private readonly ILogger<BrokerControllerSubscription> _logger;
 
-    public BrokerControllerSubscription(ISubscriptionHandler subscriptionHandler)
+    public BrokerControllerSubscription(ISubscriptionHandler subscriptionHandler, ILogger<BrokerControllerSubscription> logger)
     {
         _subscriptionHandler = subscriptionHandler;
+        _logger = logger;
     }
 
     public Task Consume(BrokerSubscriptionDto contract)
     {
+        _logger.LogInformation($"Subscription attempt of queue {contract.QueueName}");
+
         ISubscription subscription = Subscription.From(contract);
         // creates subscription
         // creates queue
