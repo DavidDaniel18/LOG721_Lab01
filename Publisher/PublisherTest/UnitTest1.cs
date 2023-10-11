db1c6f42ba4f33d16b39090602f2b4b223b947a9
JeremieB <jeremie.bergeron.1@ens.etsmtl.ca>
2023-10-10 21:54:15 -0400

    [UnitTest] Add Post_MultipleXmlValidData_ReturnsOkResult

--- Publisher/PublisherTest/UnitTest1.cs
+++ Publisher/PublisherTest/UnitTest1.cs
@@ -110,6 +110,33 @@ namespace PublisherTest
             }
         }
 
+        [TestMethod]
+        public async void Post_MultipleXmlValidData_ReturnsOkResult()
+        {
+
+            var clientNodeController = new RestClient($"http://{hostname}:{port}");
+
+            var requestRouting = new RestRequest("/Publisher", Method.Post);
+
+            requestRouting.AddQueryParameter("nbr_message", 20);
+            requestRouting.AddQueryParameter("routing_key", "weather/montreal/temperature");
+
+            requestRouting.AddXmlBody(new
+            {
+                message = "Test",
+            });
+
+            RestResponse response = clientNodeController.Execute(requestRouting);
+
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                string content = response.Content;
+
+                // TODO : Vérifier le contenu de la réponse et que les 20 messages ont été reçu
+            }
+        }
+
+
 
     }
 }
